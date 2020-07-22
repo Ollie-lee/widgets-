e91c6be4ea44f59f799d468353c364cd2dc5cd76
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-22 10:20:09 +1000

    add some hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+const items = [
+    {
+        title: 'What is React',
+        content:'React is a front-end javascript framework'
+    },
+    {
+        title:'Why use React',
+        content:'React is a fav JS library between engineers'
+    },
+    {
+        title:'How do you use React',
+        content:'You use React by creating components'
+    }
+]
 
 function App() {
     return (
         <div>
-            test
+            <Accordion items={items} />
         </div>
     )
 }
