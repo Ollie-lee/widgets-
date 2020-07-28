3f3900360013ec4ae1823997227076dea4e48127
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-28 14:52:38 +1000

    completed

--- src/App.js
+++ src/App.js
@@ -3,40 +3,67 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 
-// const options = [
-//     {
-//         label: 'The Color Red',
-//         value: 'red'
-//     },
-//     {
-//         label: 'The Color Green',
-//         value: 'green'
-//     },
-//     {
-//         label: 'A Shade of Blue',
-//         value: 'blue'
-//     }
-// ]
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front-end javascript framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React is a fav JS library between engineers'
+    },
+    {
+        title: 'How do you use React',
+        content: 'You use React by creating components'
+    }
+]
+
+const colorOptions = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+]
+
+
+
 
 function App() {
-    // const [selected, setSelected] = useState(options[0])
-    // const [showDropdown, setShowDropdown] = useState(true)
+    const [selected, setSelected] = useState(colorOptions[0])
 
 
     return (
         <div>
-            {/* <Accordion items={items} /> */}
-            {/* <Search /> */}
-            {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button> */}
-            {/* {
-                showDropdown && <Dropdown
+            <Header />
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    label='Select a color'
+                    options={colorOptions}
                     selected={selected}
                     onSelectedChange={setSelected}
-                    options={options}
                 />
-            } */}
-            <Translate />
+
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
         </div>
     )
 }
